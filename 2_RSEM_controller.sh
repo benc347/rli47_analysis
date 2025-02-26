c75c2eceadf76cc45526a547bfc4b61258341190
Ben Tibbs-Cortes <42550734+benc347@users.noreply.github.com>
2025-02-26 16:33:53 -0600

    Add files via upload

---
+++ 2_RSEM_controller.sh
@@ -0,0 +1,32 @@
+#!/bin/bash 
+
+#SBATCH --time=48:00:00					##(day-hour:minute:second) sets the max time for the job
+#SBATCH --cpus-per-task=8	 			##request number of cpus
+#SBATCH --mem=16G						##max ram for the job
+
+#SBATCH --nodes=1						##request number of nodes (always keep at 1)
+#SBATCH --mail-user=bienvenido.tibbs-cortes@usda.gov		##email address to mail specified updates to
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END					##these say under what conditions do you want email updates
+#SBATCH --mail-type=FAIL
+#SBATCH --output="00_rsem_master_slurmlog_%j"		##names what slurm logfile will be saved to 
+
+module load rsem
+module load bowtie2
+
+#accept user input
+ref=$1
+outdir=${2%/}
+mkdir $outdir
+
+#runs RSEM on all single-end fastq files
+#first calculates read length mean and sd for input
+
+for rep in *clean.fastq; do
+	fragmean=$(awk 'BEGIN { t=0.0;sq=0.0; n=0;} ;NR%4==2 {n++;L=length($0);t+=L;sq+=L*L;}END{print m=t/n;}' $rep)
+	fragsd=$(awk 'BEGIN { t=0.0;sq=0.0; n=0;} ;NR%4==2 {n++;L=length($0);t+=L;sq+=L*L;}END{m=t/n; print sqrt(sq/n-m*m);}' $rep)
+	#echo $fragmean $fragsd
+	
+	sbatch quantify_RSEM.sh $rep $ref ${outdir}/${rep%*_clean.fastq} $fragmean $fragsd
+	
+done
---
+++ 3_quantify_RSEM.sh
@@ -0,0 +1,43 @@
+#!/bin/bash 
+
+#SBATCH --time=48:00:00					##(day-hour:minute:second) sets the max time for the job
+#SBATCH --cpus-per-task=40	 			##request number of cpus
+#SBATCH --mem=64G						##max ram for the job
+
+#SBATCH --nodes=1						##request number of nodes (always keep at 1)
+#SBATCH --mail-user=bienvenido.tibbs-cortes@usda.gov		##email address to mail specified updates to
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END					##these say under what conditions do you want email updates
+#SBATCH --mail-type=FAIL
+#SBATCH --output="00_rsem_quantify_slurmlog_%j"		##names what slurm logfile will be saved to 
+
+module load rsem
+module load bowtie2
+module load samtools
+
+#accept user input
+rep=$1
+ref=$2
+outdir=$3
+fragmean=$4
+fragsd=$5
+
+# Uses an RSEM index to quantify reads
+# 	ref is the index name
+# -p should equal number of tasks/node
+# Have to use bowtie2 to align
+# --no-bam-output means no bam file is output, but a bam file will still be generated during alignment
+# reverse strandedness because of library prep
+# --estimate-rspd assesses positional biases (RSEM)
+# --append-names keeps gene and transcript names associated with data, necessary for DeSeq2
+# fragmean and fragsd are the overall read average
+
+rsem-calculate-expression -p 40 \
+						--bowtie2 \
+						--no-bam-output \
+						--strandedness reverse \
+						--estimate-rspd \
+						--append-names \
+						--fragment-length-mean $fragmean \
+						--fragment-length-sd $fragsd \
+						$rep $ref $outdir
