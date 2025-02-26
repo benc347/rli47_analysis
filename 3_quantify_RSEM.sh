54c0c6bb073d122e91098bdb8e326edeaf671a07
Ben Tibbs-Cortes <42550734+benc347@users.noreply.github.com>
2025-02-26 16:47:18 -0600

    Update 3_quantify_RSEM.sh

--- 3_quantify_RSEM.sh
+++ 3_quantify_RSEM.sh
@@ -5,37 +5,45 @@
 #SBATCH --mem=64G						##max ram for the job
 
 #SBATCH --nodes=1						##request number of nodes (always keep at 1)
-#SBATCH --mail-user=bienvenido.tibbs-cortes@usda.gov		##email address to mail specified updates to
+#SBATCH --mail-user=YOUR_EMAIL_HERE			##email address to mail specified updates to
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END					##these say under what conditions do you want email updates
 #SBATCH --mail-type=FAIL
 #SBATCH --output="00_rsem_quantify_slurmlog_%j"		##names what slurm logfile will be saved to 
 
+#this script runs RSEM map and quantify reads against an annotated genome
+	#for single end reads ONLY
+ 	#script is designed to be invoked by 2_RSEM_controller.sh
+  		#can be run on its own though
+ 
+#load modules
 module load rsem
 module load bowtie2
 module load samtools
 
 #accept user input
+	#rep - fastq file to map against the reference
+ 	#ref - RSEM index created using 1_RSEM_index.sh
+  	#outdir - output directory created by 2_RSEM_controller.sh
+   	#fragmean - mean read length calculated by 2_RSEM_controller.sh
+    	#fragsd - standard deviation of read length calculated by 2_RSEM_controller.sh
+     	#strand - strandedness of the library prep. depending on the prep, reads may map to either the forward or opposite strand in the genome
 rep=$1
 ref=$2
 outdir=$3
 fragmean=$4
 fragsd=$5
+strand=$6
 
-# Uses an RSEM index to quantify reads
-# 	ref is the index name
-# -p should equal number of tasks/node
 # Have to use bowtie2 to align
 # --no-bam-output means no bam file is output, but a bam file will still be generated during alignment
-# reverse strandedness because of library prep
 # --estimate-rspd assesses positional biases (RSEM)
 # --append-names keeps gene and transcript names associated with data, necessary for DeSeq2
-# fragmean and fragsd are the overall read average
 
 rsem-calculate-expression -p 40 \
 						--bowtie2 \
 						--no-bam-output \
-						--strandedness reverse \
+						--strandedness $strand \
 						--estimate-rspd \
 						--append-names \
 						--fragment-length-mean $fragmean \
